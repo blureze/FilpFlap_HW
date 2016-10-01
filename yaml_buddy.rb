9ccee85b3b78c466a8e18c7b67a899d9b0707dcb
blureze <blureze@gmail.com>
2016-10-02 02:45:44 +0800

    first refactoring of yaml_buddy.rb

--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,11 +1,11 @@
-# Module that can be included (mixin) to take and output Yaml data
 require 'yaml'
 
+# Module that can be included (mixin) to take and output Yaml data
 module YamlBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_yaml(yaml)
-    @data = YAML::load(yaml)
+    @data = YAML.load(yaml)
   end
 
   # to_yaml: converts any data in @data into a yaml string.
