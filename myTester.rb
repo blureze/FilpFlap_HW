15eaccd3dee65fd42fb65f8e5281e310036ae1c8
blureze <blureze@gmail.com>
2016-10-02 01:13:04 +0800

    finish tsv_buddy.rb

--- myTester.rb
+++ myTester.rb
@@ -1,6 +1,7 @@
+require_relative 'tsv_buddy'
+
 class Tester
   include TsvBuddy
 end
 
 t = Tester.new.take_tsv(File.read('data/survey.tsv'))
-t.take_tsv
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,4 +1,6 @@
 # Module that can be included (mixin) to take and output TSV data
+require 'yaml'
+
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
@@ -13,15 +15,15 @@ module TsvBuddy
 
     @data = value.map do |row|
       row.map.with_index do |cell, i|
-        [headers[i], cell].to_h
-      end
+        [headers[i], cell]
+      end.to_h
     end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-    output = 'date\tstudent_id\tbest_language\tapp_experience\ttech_experience'
+    @output = 'date\tstudent_id\tbest_language\tapp_experience\ttech_experience'
     @data.each do |elem|
       date = elem[0]
       student_id = elem[1]
@@ -30,7 +32,7 @@ module TsvBuddy
       app_experience = elem[4]
       tech_experience = elem[5]
 
-      output += date + "\t" + student_id + "\t" + languages + "\t" + best_language + "\t" + app_experience + "\t" + tech_experience + "\n"
+      @output += date + "\t" + student_id + "\t" + languages + "\t" + best_language + "\t" + app_experience + "\t" + tech_experience + "\n"
     end
   end
 end
