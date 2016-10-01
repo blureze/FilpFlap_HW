43ca9eaf2325dd3e4ccb4be8d947d2765044ca68
blureze <blureze@gmail.com>
2016-10-02 01:59:34 +0800

    finish yaml_buddy.rb

--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,14 @@
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  # take_tsv: converts a String with TSV data into @data
+  # parameter: tsv - a String in TSV format
+  def take_yaml(yaml)
+    @data = YAML::load(yaml)
+  end
+
+  # to_yaml: converts any data in @data into a yaml string.
+  # returns: String in TSV format
+  def to_yaml
+    @data.to_yaml
+  end
+end
